9c4a55ba9428044cd31529b80660d9bbce384840
mboyas <michael.boyas@kellogg.northwestern.edu>
2022-03-11 17:04:19 +0000

    authenticate in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,26 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # authenticate the user
+    @user = User.find_by({ email: params["email"] })
+    if @user
+      if BCrypt::Password.new(@user.password) == params["password"]
+        session[:user_id] = @user.id
+        flash[:notice] = "Welcome, #{@user.username}."
+        redirect_to "/places"
+      else
+        flash[:notice] = "Login Unsuccessful"
+        redirect_to "/sessions/new"
+      end
+    else
+      flash[:notice] = "Login Unsuccessful"
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to "/sessions/new"
   end
 end
   
